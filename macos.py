7098f8b62c6d6aecd4cc9d2fd949f3074949188d
Sasha Mattison <sashastoolbox@gmail.com>
2025-12-15 08:30:25 -0800

    Code cleaning
    Changed how some code executes and improved the structure and readability of some functions. Added a new macos.py and linux.py for execution on their respective systems.

---
+++ macos.py
@@ -0,0 +1,58 @@
+import subprocess
+import platform
+import shutil
+
+#Package list goes like so: Basic, Advanced, Developer
+macos_packages = [["fastfetch", "mactop"], ["fastfetch"], ["mactop"]]
+
+def has_brew():
+    return shutil.which("brew") is not None
+
+def prepare_macos():
+    print("Checking user permissions...")
+    run("sudo echo 'User has adequete permissions'")
+    if has_brew():
+        print("Homebrew is installed:")
+        print("Updating...")
+        run("brew update && brew upgrade")
+        print("System preparation is complete...")
+    else:
+        print("Homebrew is NOT installed:")
+        print("Installing...")
+        print("Checking user permissions...")
+        run("sudo echo 'User has adequete permissions'")
+        run('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+        run("brew update && brew upgrade")
+
+def install_on_macos(selection):
+    print("Installing packages...")
+    for i in range(len(macos_packages[selection])):
+        run("brew install " + macos_packages[selection][i])
+
+def macos_chest_select_install():
+    print("Which chest (collection of packages) would you like to install?")
+    print("Basic: 1")
+    print("Advanced: 2")
+    print("Developer: 3 (NOTE: may take a while)")
+    selec = str(input("Enter choice: "))
+    if selec in ("1", "2", "3"):
+        if selec == "1":
+            install_on_macos(1)
+        elif selec == "2":
+            install_on_macos(2)
+        else:
+            install_on_macos(3)
+    else:
+        print("Invalid input, try again")
+        macos_chest_select_install()
+
+def run(cmd):
+    try:
+        subprocess.run(cmd, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Ignored non-critical error: {e}")
+
+#Program execution below
+
+prepare_macos()
+macos_chest_select_install()
--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import subprocess
 import platform
-import time
-import shutil
-
-macos_packages_basic = ["fastfetch", "mactop"]
-macos_packages_advanced = ["fastfetch"]
-macos_packages_developer = ["mactop"]
-
-
+import macos
+import linux
 
 os = platform.system()
 
@@ -17,94 +11,15 @@ if os == "Darwin":
 def exec_on_os():
     if os in ("MacOS", "Linux"):
         print("Executing on " + os + "...")
-        time.sleep(0.1)
     else:
         raise SystemExit("Unsupported OS")
 
-def has_brew():
-    return shutil.which("brew") is not None
 
-def prepare_macos():
-    print("Checking user permissions...")
-    run("sudo echo 'User has adequete permissions'")
-    if has_brew():
-        print("Homebrew is installed:")
-        print("Updating...")
-        run("brew update && brew upgrade")
-        print("System preparation is complete...")
-    else:
-        print("Homebrew is NOT installed:")
-        print("Installing...")
-        print("Checking user permissions...")
-        run("sudo echo 'User has adequete permissions'")
-        run('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
-        run("brew update && brew upgrade")
+#Program execution below
 
-def install_on_macos1():
-    print("Installing packages...")
-    for i in range(len(macos_packages_basic)):
-        run("brew install " + macos_packages_basic[i])
-
-def install_on_macos2():
-    print("Installing packages...")
-    for i in range(len(macos_packages_advanced)):
-        run("brew install " + macos_packages_advanced[i])
-
-def install_on_macos3():
-    print("Installing packages...")
-    run("xcode-select --install")
-    for i in range(len(macos_packages_developer)):
-        run("brew install " + macos_packages_developer[i])
-
-def macos_chest_select_install():
-    print("Which chest (collection of packages) would you like to install?")
-    print("Basic: 1")
-    print("Advanced: 2")
-    print("Developer: 3 (NOTE: may take a while)")
-    selec = str(input("Enter choice: "))
-    if selec in ("1", "2", "3"):
-        if selec == "1":
-            install_on_macos1()
-        elif selec == "2":
-            install_on_macos2()
-        else:
-            install_on_macos3()
-    else:
-        print("Invalid input, try again")
-        macos_chest_select_install()
-
-def get_linux_distro():
-    distro = {}
-    try:
-        with open("/etc/os-release") as f:
-            for line in f:
-                line = line.strip()
-                if not line or "=" not in line:
-                    continue
-                k, v = line.split("=", 1)
-                distro[k] = v.strip('"')
-    except FileNotFoundError:
-        return None
-
-    return {
-        "id": distro.get("ID"),
-        "name": distro.get("NAME"),
-        "like": distro.get("ID_LIKE"),
-        "version": distro.get("VERSION_ID"),
-    }
-
-
-def run(cmd):
-    try:
-        subprocess.run(cmd, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Ignored non-critical error: {e}")
-        
 exec_on_os()
 
 if os == "Linux":
-    get_linux_distro()
-    print("Linux is unsuported at this time :( -- (Coming soon!)")
+    subprocess.run(linux)
 else:
-    prepare_macos()
-    macos_chest_select_install()
+    subprocess.run(macos.py)
